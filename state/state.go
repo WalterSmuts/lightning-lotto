WalterSmuts/lightning-lotto

commit 29c8a683922461805cdabe46b99cd0972784f719

Split addTicket into safe and unsafe variant

state/state.go
@@ -105,6 +105,10 @@ func (n *State) CountdownTimerChannel() <-chan time.Time {
 func (n *State) addTicket(ticket *Ticket) {
 	n.mu.Lock()
 	defer n.mu.Unlock()
+	n.addTicket(ticket)
+}
+
+func (n *State) addTicketUnsafe(ticket *Ticket) {
 	n.tickets = append(n.tickets, ticket)
 	n.pot += ticket.AmountSats
 }

commit 6a44f5922a19af1354e8c7ac3153ecd7fd2a47bd

Fix deadlock causing bug
Introduced in 29c8a683922461805cdabe46b99cd0972784f719

state/state.go
@@ -106,7 +106,7 @@ func (n *State) CountdownTimerChannel() <-chan time.Time {
 func (n *State) addTicket(ticket *Ticket) {
 	n.mu.Lock()
 	defer n.mu.Unlock()
-	n.addTicket(ticket)
+	n.addTicketUnsafe(ticket)
 }
 
 func (n *State) addTicketUnsafe(ticket *Ticket) {